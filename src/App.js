0d3b4566f3e6a975bef647a19276683c200534d4
Aditya Pratap Singh <adityapratap1133@gmail.com>
2025-04-08 15:47:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Camera from './components/Camera';
 import SentenceDisplay from './components/SentenceDisplay';
 
 function App() {
-  const [sentence, setSentence] = useState("");
-
-  const handleReset = () => {
-    setSentence("");
-  };
-
-  const handlePrediction = (gesture, newSentence) => {
-    setSentence(newSentence);
-  };
-
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Hand Gesture Translation App</h1>
-      <SentenceDisplay 
-        sentence={sentence} 
-        onReset={handleReset} 
-      />
-      <Camera onPrediction={handlePrediction} />
+      <SentenceDisplay />
+      <Camera />
     </div>
   );
 }
